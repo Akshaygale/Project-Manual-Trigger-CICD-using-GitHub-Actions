4c95cbe54231d5081150da5025335cf093b9ddda
Ubuntu <ubuntu@ip-172-31-41-116.ap-south-1.compute.internal>
2025-12-25 09:30:22 +0000

    py-change

--- app.py
+++ app.py
@@ -1,8 +1,14 @@
 from flask import Flask
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "CI/CD Deployed Successfully 🚀"
+    now = datetime.datetime.now()
+    return f"""
+    <h1>CI/CD Auto Deployment Successful 🚀</h1>
+    <p>Server Time: {now}</p>
+    """
 
 app.run(host="0.0.0.0", port=5000)
