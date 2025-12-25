8fa077443ec6443822ca6656bb76b2fd213b1e8d
Ubuntu <ubuntu@ip-172-31-41-116.ap-south-1.compute.internal>
2025-12-25 09:35:23 +0000

    version

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ import datetime
 
 app = Flask(__name__)
 
+with open("VERSION") as f:
+    VERSION = f.read().strip()
+
 @app.route("/")
 def home():
     now = datetime.datetime.now()
     return f"""
     <h1>CI/CD Auto Deployment Successful 🚀</h1>
+    <h3>Version: {VERSION}</h3>
     <p>Server Time: {now}</p>
     """
 
